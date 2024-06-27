c9afbbc28d87087247964c66cf99632b6f2c31d1
Aayush Chaudhary <acaayush1111@gmail.com>
2024-06-27 12:29:14 +0530

    non workflow

--- scripts/new_post.sh
+++ scripts/new_post.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+# Get the current date and time
 DATE=$(date "+%d-%m-%Y %H:%M")
 
-echo "Enter content: (Ctrl+D to end)"
+
+echo "Post title: "
+POST_TITLE=$(cat)
+
+# Prompt for the post content
+echo "Enter your post content (press Ctrl+D when finished):"
 POST_CONTENT=$(cat)
 
 # Escape special characters in the post content
 ESCAPED_CONTENT=$(echo "$POST_CONTENT" | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 
+# Read the existing posts
 POSTS_FILE="../posts/posts.json"
 if [ ! -f "$POSTS_FILE" ]; then
     echo "[]" > "$POSTS_FILE"
 fi
 POSTS=$(cat "$POSTS_FILE")
 
+# Remove the opening and closing brackets
+POSTS=${POSTS#[}
+POSTS=${POSTS%]}
+
 # Add the new post to the beginning of the array
-NEW_POSTS=$(echo "[$DATE,$ESCAPED_CONTENT]$POSTS" | sed 's/\]\[/,/')
+NEW_POSTS="[{\"title\":\"$POST_TITLE\",\"date\":\"$DATE\",\"content\":\"$ESCAPED_CONTENT\"}${POSTS:+,$POSTS}]"
 
 # Write the updated posts back to the file
 echo "$NEW_POSTS" > "$POSTS_FILE"
